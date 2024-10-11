8fbb4da06a22388bb47cf2ec87e4930924a1add8
MIMIK <78480793+I-vanik@users.noreply.github.com>
2024-10-11 23:39:15 +0300

    Add files via upload

--- script/modal.js
+++ script/modal.js
@@ -1,17 +1,22 @@
-let modal = document.getElementById('modalCorn')
-let btn = document.getElementById('modalCornBtn')
-let span = document.getElementsByClassName('modal-corn-close')[0];
+let btns = document.querySelectorAll("*[modal-btn]");
 
-btn.onclick = function() {
-    modal.style.display = "block"
-}
-span.onclick = function() {
-    modal.style.display = "none"
-
-}
-
-window.onclick = function(event) {
-    if (event.target == modal) {
+for(let i = 0; i<btns.length; i++) {
+  btns[i].addEventListener('click', function () {
+    let name = btns[i].getAttribute('modal-btn');
+    let modal = document.querySelector("[modal-window='"+name+"']");
+    modal.style.display = "block";
+    let close = modal.querySelector(".modal-close")
+    close.addEventListener('click', function () {
       modal.style.display = "none";
+    })
+  })
+}
+
+window.onclick = function (e) {
+  if(e.target.hasAttribute('modal-window')) {
+    let modals = document.querySelectorAll("*[modal-window]");
+    for( let i = 0; i<modals.length; i++) {
+      modals[i].style.display = "none";
     }
   }
+}
